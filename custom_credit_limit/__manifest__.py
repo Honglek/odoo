776a7ac535d79aa17c1817ed6c8c2fe790a88128
Honglek <160992141+Honglek@users.noreply.github.com>
2025-02-03 09:51:04 +0700

    Add files via upload

---
+++ custom_credit_limit/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Custom Credit Limit',
+    'version': '1',
+    'depends': ['base','sale'],
+    'data'  : [
+        'views/sale_views.xml',
+        'security/sale_security.xml'
+    ],
+    'test': [],
+    'installable': True,
+    'auto_install': True, 
+    'license': 'LGPL-3',
+}
